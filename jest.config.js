cd8b5366503d8518db760383469c7a64fc7679f1
lengleman-spec <191269315+lengleman-spec@users.noreply.github.com>
2026-03-29 18:56:14 -0400

    Add logic to count the hits and return true if the ship is sunk or false otherwise.

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,6 @@
-// jest.config.js
 export default {
   testEnvironment: "node",
   transform: {
     "^.+\\.js$": "babel-jest",
   },
-  extensionsToTreatAsEsm: [".js"], // important for ESM
 };
--- src/ship.test.js
+++ src/ship.test.js
@@ -1,3 +1,23 @@
-test("adds numbers", () => {
-  expect(1 + 1).toBe(2);
+import { Ship } from "../src/ship.js";
+
+describe("Ship class", () => {
+  test("hit() increments the hits counter", () => {
+    const ship = new Ship(3);
+    expect(ship.hits).toBe(0);
+    ship.hit();
+    expect(ship.hits).toBe(1);
+  });
+
+  test("isSunk() returns false when the number of hits is less than its length", () => {
+    const ship = new Ship(3);
+    expect(ship.isSunk()).toBe(false);
+  });
+
+  test("isSunk returns true when the number of hits is greater than or equal to its length", () => {
+    const ship = new Ship(3);
+    ship.hit();
+    ship.hit();
+    ship.hit();
+    expect(ship.isSunk()).toBe(true);
+  });
 });
