2c987b739032f774e5dee87537c9c9196889ee1c
lengleman-spec <191269315+lengleman-spec@users.noreply.github.com>
2026-03-30 09:22:35 -0400

    Create gameboard class and placeShip and receiveAttack functions.

--- src/ship.js
+++ src/ship.js
@@ -12,3 +12,37 @@ export class Ship {
     return this.hits >= this.length; // returns true/false
   }
 }
+
+class Gameboard {
+  constructor() {
+    this.ships = [];
+    this.missedAttacks = [];
+  }
+
+  placeShip(ship, coordinates) {
+    this.ships.push({
+      ship: ship,
+      coordinates: coordinates,
+    });
+  }
+
+  receiveAttack(coordinate) {
+    let hit = false; // Track whether we hit a ship
+
+    for (let shipObj of this.ships) {
+      if (
+        shipObj.coordinates.some(
+          (coord) => coord[0] === coordinate[0] && coord[1] === coordinate[1],
+        )
+      ) {
+        shipObj.ship.hit();
+        hit = true;
+        break;
+      }
+    }
+
+    if (!hit) {
+      this.missedAttacks.push(coordinate);
+    }
+  }
+}
